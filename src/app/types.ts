a887f4a9dd0b0b9a431ef6902b5a1ec983be9148
cyrielletouton <cyrielle.t@hotmail.fr>
2022-03-07 11:08:34 +0100

    details done + css updated

--- src/app/types.ts
+++ src/app/types.ts
@@ -5,4 +5,16 @@ export class Movie {
     poster_path!: string;
     vote_average!: number;
     runtime!: number;
+    release_date!: Date;
+    spoken_languages!: Language[] ;
+    genres!: Genre[];
   }
+
+export class Language{
+  name!: string;
+}
+
+export class Genre {
+  id!: number;
+  name!: string;
+}
